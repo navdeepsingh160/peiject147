c247f6dc67cec780e9595d838ac680ff00310a1c
navdeepsingh160 <118738353+navdeepsingh160@users.noreply.github.com>
2024-06-21 12:53:07 +0530

    Add files via upload

---
+++ n IMAGE_VIEWER.py
@@ -0,0 +1,12 @@
+import tkinter
+import ImageTK
+import PIL
+
+
+configure(background="black")
+
+Label(background = "black")
+
+place(Top) 
+
+img_path("/Users/macbookair/Desktop/project147/image.jpeg")
